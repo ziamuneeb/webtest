5b8969bc9d21a019583127d1b1d4c0a5f823d595
ziamuneeb <52719471+ziamuneeb@users.noreply.github.com>
2022-07-22 12:58:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def homepage():
-    return "<h1>Hi, I am muneeb <h1>"
+    return "<h1>Hi<h1>"
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = 80)
